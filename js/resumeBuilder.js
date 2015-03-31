954f9466160dfcdd3082916faac483e5787deb82
Amr <amr@Amrs-Mac.local>
2015-03-31 20:19:22 +0200

    apepnded name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,5 @@
-$("#main").append("Amr");
+//$("#main").append("Amr");
+var formattedName = HTMLheaderName.replace("%data%", "Amr M. Elsayed");
+var formattedRole = HTMLheaderRole.replace("%data%", "Front-End Eveloper");
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
